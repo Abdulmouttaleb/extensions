903b2bb06c1a23f1dff598902ca5605068749993
Abdulmouttaleb <139283950+Abdulmouttaleb@users.noreply.github.com>
2024-09-30 06:51:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Fetch the JSON data (replace with your actual GitHub repo URL)
-fetch('https://github.com/Abdulmouttaleb/extensions/blob/main/permissions.json')
+fetch('https://github.com/Abdulmouttaleb/extensions/raw/refs/heads/main/permissions.json')
   .then(response => response.json())
   .then(data => {
     // Populate the extensions table
